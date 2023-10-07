a8a133133f8075fe307418391327283832b60a19
Abhikappana <abhikappana@gmail.com>
2023-10-07 22:10:53 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,6 +119,30 @@ closeBtn.addEventListener("click", () => {
     navigation.classList.remove("active");
 });
 
+// ======= Form Submition==========
+   
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyHQO-sdxBnZUG8YL0NwOPLqrmJ7LRYHSD85e5LVAaq1xuGDCWHUX4z8TAS_rpstRO4/exec';
+const form = document.forms['submit-to-google-sheet']
+const message = document.getElementById("message")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  if(validateForm()){
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            message.innerHTML = "Message sent"
+            alert('Form Submitted');
+            window.location.reload();
+            setTimeout(()=>{
+                message.innerHTML = " "
+            },3000)
+        })
+        .catch(error => console.error('Error!', error.message))
+  }else{
+    alert('validation failed');
+  }
+
+})
 
 //========= contact validation==========
 function validateForm() {
@@ -163,30 +187,6 @@ function validateForm() {
 
     return true;
 }
-// ======= Form Submition==========
-   
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbyHQO-sdxBnZUG8YL0NwOPLqrmJ7LRYHSD85e5LVAaq1xuGDCWHUX4z8TAS_rpstRO4/exec';
-    const form = document.forms['submit-to-google-sheet']
-    const message = document.getElementById("message")
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      if(validateForm()){
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                message.innerHTML = "Message sent"
-                alert('Form Submitted');
-                window.location.reload();
-                setTimeout(()=>{
-                    message.innerHTML = " "
-                },3000)
-            })
-            .catch(error => console.error('Error!', error.message))
-      }else{
-        alert('validation failed');
-      }
-
-    })
 
 //======Scroll reveal animations=====
 // Common reveal options to create reveal  animations
